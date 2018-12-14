d08712cafc8516b55038ceb8b945958b51e6d5fe
Grant Wuerker <gwuerker@googlemail.com>
2018-12-13 20:25:31 -0600

    brought in external contracts for solc 0.5.0

---
+++ contracts/external/DutchExchangeInterface.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+import "./PriceOracleInterface.sol";
+
+interface DutchExchange {
+    function getPriceOfTokenInLastAuction(
+        address token
+    ) external view returns (uint num, uint den);
+    
+    function getPriceInPastAuction(
+        address token,
+        address token2,
+        uint auctionIndex
+    ) external view returns (uint num, uint den);
+    
+    function getAuctionIndex(
+        address token,
+        address token2
+    ) external view returns (uint auctionIndex);
+}
