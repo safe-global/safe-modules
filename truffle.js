a4efae564c580ebae76ac2ee69534121e99b03f7
Denis Granha <denisgranha@gmail.com>
2019-02-25 16:05:27 +0100

    refactored truffle.js file, added deploy commands to package.json and first networks.json of master copies

--- truffle.js
+++ truffle.js
@@ -1,37 +1,52 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const args = require('yargs').argv
+const truffleConfig = require('@gnosis.pm/util-contracts/src/util/truffleConfig')
 
-const mnemonic = process.env.MNEMONIC || args.mnemonic
-const token = process.env.INFURA_TOKEN
+const DEFAULT_GAS_PRICE_GWEI = 5
+const DEFAULT_GAS_LIMIT = 6721975
+const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
+
+// Load env vars
+require('dotenv').config()
+
+// Get the mnemonic
+const privateKey = process.env.PK
+let mnemonic = process.env.MNEMONIC
+if (!privateKey && !mnemonic) {
+  mnemonic = DEFAULT_MNEMONIC
+}
+
+// Solc
+const compatibilityTruffle4 = true
+let solcUseDocker, solcVersion
+if (!compatibilityTruffle4) {
+  // Use truffle 5
+  solcUseDocker = process.env.SOLC_USE_DOCKER === 'true' || false
+  solcVersion = '0.5.0'
+}
+
+// Gas price
+const gasPriceGWei = process.env.GAS_PRICE_GWEI || DEFAULT_GAS_PRICE_GWEI
+
+// Gas limit
+const gas = process.env.GAS_LIMIT || DEFAULT_GAS_LIMIT
+
+// Allow to add an aditional network (useful for docker-compose setups)
+//  i.e. NETWORK='{ "name": "docker", "networkId": "99999", "url": "http://rpc:8545", "gas": "6700000", "gasPrice": "25000000000"  }'
+let aditionalNetwork = process.env.NETWORK ? JSON.parse(process.env.NETWORK) : null
 
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 6000000
-    },
-    rinkeby: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/' + token, 0, 10)
-      },
-      network_id: '4',
-      gas: 6700000,
-      gasPrice: 3000000000, // 1 Gwei
-    },
-    mainnet: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/' + token)
-      },
-      network_id: '1',
-      gas: 6700000,
-      gasPrice: 25000000000, // 25 Gwei
-    }
-  },
-  solc: {
-    optimizer: {
-      enabled: false
-    },
-  },
-};
+  ...truffleConfig({
+    mnemonic,
+    privateKey,
+    gasPriceGWei,
+    gas,
+    aditionalNetwork,
+    optimizedEnabled: false,
+    solcUseDocker,
+    solcVersion,
+    compatibilityTruffle4
+  }),
+  // https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
+  mocha: {
+
+  }
+}
