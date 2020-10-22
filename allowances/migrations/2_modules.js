c63267957d12143fc3744c714b5390073c5434ba
Richard Meissner <msc.meissner@gmail.com>
2020-10-23 01:00:04 +0200

    Use singleton deployer

--- allowances/migrations/2_modules.js
+++ allowances/migrations/2_modules.js
@@ -1,5 +1,8 @@
+const { deployTruffleContract } = require('@gnosis.pm/singleton-deployer-truffle');
 const AllowanceModule = artifacts.require("AllowanceModule");
 
 module.exports = function(deployer) {
-  deployer.deploy(AllowanceModule);
+  deployer.then(async () => {
+    await deployTruffleContract(web3, AllowanceModule);
+  })
 };
