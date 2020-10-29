66aedc0adc0e644be9d96a3e57c32dd1000ed69b
Denis Granha <denisgranha@gmail.com>
2020-10-29 10:11:47 +0100

    adapt migration for cdp-module

--- maker_cdp/migrations/2_modules.js
+++ maker_cdp/migrations/2_modules.js
@@ -1,5 +1,19 @@
-const AllowanceModule = artifacts.require("AllowanceModule");
+const VaultLiquidationProtectionModule = artifacts.require("VaultLiquidationProtectionModule");
 
 module.exports = function(deployer) {
-  deployer.deploy(AllowanceModule);
+  /**
+   * All params are provided by command line
+   * maker_vat_vault, 
+   * cdp_owner, 
+   * maker_dai_bridge,
+   * maker_collateral_token_bridge, 
+   * maker_colleteral_token_id, 
+   * manager,
+   * operator
+   */
+  const args = process.argv.slice()
+  if(args.length != 9){
+    console.log("you need to provide all constructor params: npx truffle migrate <maker_vat_vault> <cdp_owner> <maker_dai_bridge> <maker_colleteral_token_id> <manager> <operator>")
+  }
+  deployer.deploy(VaultLiquidationProtectionModule, args[3], args[4], args[5], args[6], args[7], args[8]);
 };
