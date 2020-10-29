725f8b7a306f525544d3f0772f54bc49f95ff5c8
Denis Granha <denisgranha@gmail.com>
2020-10-29 10:52:39 +0100

    improve migration script for cdp module

--- maker_cdp/migrations/2_modules.js
+++ maker_cdp/migrations/2_modules.js
@@ -12,8 +12,8 @@ module.exports = function(deployer) {
    * operator
    */
   const args = process.argv.slice()
-  if(args.length != 9){
-    console.log("you need to provide all constructor params: npx truffle migrate <maker_vat_vault> <cdp_owner> <maker_dai_bridge> <maker_colleteral_token_id> <manager> <operator>")
+  if(args.length != 11){
+    console.log("you need to provide all constructor params: npx truffle migrate <maker_vat_vault> <cdp_owner> <maker_dai_bridge> <maker_collateral_token_bridge> <maker_colleteral_token_id> <manager> <operator>")
   }
-  deployer.deploy(VaultLiquidationProtectionModule, args[3], args[4], args[5], args[6], args[7], args[8]);
+  deployer.deploy(VaultLiquidationProtectionModule, args[4], args[5], args[6], args[7], args[8], args[9], args[10]);
 };
--- maker_cdp/truffle-config.js
+++ maker_cdp/truffle-config.js
@@ -61,14 +61,15 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/` + token),
+      network_id: 42,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
+      networkCheckTimeout: 200000
+    },
 
     // Useful for private networks
     // private: {
