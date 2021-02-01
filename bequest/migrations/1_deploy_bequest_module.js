7315bca9c04ae8b10d219bdc3bf5b42ec24572f1
Victor Porton <porton@narod.ru>
2021-02-01 06:24:16 +0200

    new deployment

--- bequest/migrations/1_deploy_bequest_module.js
+++ bequest/migrations/1_deploy_bequest_module.js
@@ -1,8 +1,7 @@
-const { deployTruffleContract } = require('@gnosis.pm/singleton-deployer-truffle');
 const BequestModule = artifacts.require("BequestModule");
 
 module.exports = function(deployer) {
   deployer.then(async () => {
-    await deployTruffleContract(web3, BequestModule);
+    await deployer.deploy(BequestModule);
   })
 };
