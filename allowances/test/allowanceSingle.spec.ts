caf0cfe6afccba62d7d820b03faa83500a60d86f
cristovaoth <cristovao.honorato@gmail.com>
2023-08-26 10:58:27 +0200

    Add another test

--- allowances/test/allowanceSingle.spec.ts
+++ allowances/test/allowanceSingle.spec.ts
@@ -1,5 +1,7 @@
-import hre from 'hardhat'
 import { expect } from 'chai'
+import hre from 'hardhat'
+
+import { parseUnits } from 'ethers/lib/utils'
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
@@ -8,9 +10,9 @@ import { TestToken, TestToken__factory } from '../typechain-types'
 import deploySingletons from './helpers/deploySingletons'
 import deploySafeProxy from './helpers/deploySafeProxy'
 import execTransaction from './helpers/execTransaction'
-import execAllowanceTransfer from './helpers/executeAllowanceTransfer'
+import execAllowanceTransfer from './helpers/execAllowanceTransfer'
 
-describe.only('AllowanceModule Single', async () => {
+describe('AllowanceModule allowanceSingle', async () => {
   async function setup() {
     const [owner, alice, bob, spender, receiver, deployer, johndoe] =
       await hre.ethers.getSigners()
@@ -20,6 +22,10 @@ describe.only('AllowanceModule Single', async () => {
     const token = await deployTestToken(deployer)
 
     // fund the safe
+    await owner.sendTransaction({
+      to: safe.address,
+      value: parseUnits('1', 'ether'),
+    })
     await token.transfer(safe.address, 1000)
 
     // enable Allowance as mod
@@ -51,6 +57,11 @@ describe.only('AllowanceModule Single', async () => {
       receiver,
     }
   }
+
+  before(
+    async () => await hre.network.provider.request({ method: 'hardhat_reset' })
+  )
+
   it('Execute allowance with delegate', async () => {
     const { allowanceModule, safe, token, owner, alice, bob } =
       await loadFixture(setup)
@@ -60,7 +71,7 @@ describe.only('AllowanceModule Single', async () => {
      * Alice configured as spender in Allowance
      * Bob not configured as spender in any Allowance
      *
-     * Alice spends and sends to Bob
+     * Alice sends to Bob
      */
 
     expect(await safe.isModuleEnabled(allowanceModule.address)).to.equal(true)
@@ -97,7 +108,7 @@ describe.only('AllowanceModule Single', async () => {
       await allowanceModule.getTokens(safe.address, alice.address)
     ).to.deep.equal([token.address])
 
-    // load an existing alowance
+    // load an existing allowance
     let [amount, spent, minReset, lastReset, nonce] =
       await allowanceModule.getTokenAllowance(
         safe.address,
@@ -128,9 +139,9 @@ describe.only('AllowanceModule Single', async () => {
     expect(0).to.equal(await token.balanceOf(bob.address))
 
     await execAllowanceTransfer(allowanceModule, {
-      safe,
-      token,
-      to: bob,
+      safe: safe.address,
+      token: token.address,
+      to: bob.address,
       amount: 60,
       spender: alice,
     })
@@ -147,7 +158,7 @@ describe.only('AllowanceModule Single', async () => {
         token.address
       )
 
-    // see the last transfer reflected
+    // expect the last transfer to be reflected
     expect(100).to.equal(amount)
     expect(60).to.equal(spent)
     expect(0).to.equal(minReset)
@@ -179,6 +190,118 @@ describe.only('AllowanceModule Single', async () => {
     expect(0).to.equal(lastReset)
     expect(2).to.equal(nonce)
   })
+
+  it('Execute multiple ether allowance with delegate', async () => {
+    const { provider } = hre.ethers
+    const { allowanceModule, safe, owner, alice } = await loadFixture(setup)
+
+    /*
+     * Safe will contain 1 ETH
+     * Alice configured as ETH spender in Allowance
+     * Bob not configured as spender in any Allowance
+     *
+     * Alice sends to Bob
+     */
+    const bob = '0x0000000000000000000000000000000000abcdef'
+
+    // add alice as delegate
+    await execTransaction(
+      safe,
+      await allowanceModule.populateTransaction.addDelegate(alice.address),
+      owner
+    )
+
+    // create an allowance for alice
+    await execTransaction(
+      safe,
+      await allowanceModule.populateTransaction.setAllowance(
+        alice.address,
+        AddressZero, // zero means ether
+        OneEther,
+        0,
+        0
+      ),
+      owner
+    )
+
+    // load an existing allowance
+    let [amount, spent, minReset, lastReset, nonce] =
+      await allowanceModule.getTokenAllowance(
+        safe.address,
+        alice.address,
+        AddressZero
+      )
+    expect(OneEther).to.equal(amount)
+    expect(0).to.equal(spent)
+    expect(0).to.equal(minReset)
+    expect(0).to.not.equal(lastReset) // this should be set to inti time
+    expect(1).to.equal(nonce)
+
+    expect(OneEther).to.equal(await provider.getBalance(safe.address))
+    expect(0).to.equal(await provider.getBalance(bob))
+
+    // send 0.001 to bob using alice's allowance
+    await execAllowanceTransfer(allowanceModule, {
+      safe: safe.address,
+      token: AddressZero,
+      to: bob,
+      amount: parseUnits('0.001', 'ether'),
+      spender: alice,
+    })
+
+    expect(parseUnits('0.999', 'ether')).to.equal(
+      await provider.getBalance(safe.address)
+    )
+    expect(parseUnits('0.001', 'ether')).to.equal(
+      await provider.getBalance(bob)
+    )
+
+    // load Alice's Allowance
+    ;[amount, spent, minReset, lastReset, nonce] =
+      await allowanceModule.getTokenAllowance(
+        safe.address,
+        alice.address,
+        AddressZero
+      )
+
+    // expect the last transfer to be reflected
+    expect(parseUnits('1', 'ether')).to.equal(amount)
+    expect(parseUnits('0.001', 'ether')).to.equal(spent)
+    expect(0).to.equal(minReset)
+    expect(lastReset.gt(0)).to.be.true
+    expect(2).to.equal(nonce)
+
+    // send 0.001 more
+    await execAllowanceTransfer(allowanceModule, {
+      safe: safe.address,
+      token: AddressZero,
+      to: bob,
+      amount: parseUnits('0.001', 'ether'),
+      spender: alice,
+    })
+
+    expect(parseUnits('0.998', 'ether')).to.equal(
+      await provider.getBalance(safe.address)
+    )
+    expect(parseUnits('0.002', 'ether')).to.equal(
+      await provider.getBalance(bob)
+    )
+
+    // load Alice's Allowance
+    ;[amount, spent, minReset, lastReset, nonce] =
+      await allowanceModule.getTokenAllowance(
+        safe.address,
+        alice.address,
+        AddressZero
+      )
+
+    // expect the last transfer to be reflected
+    expect(parseUnits('1', 'ether')).to.equal(amount)
+    expect(parseUnits('0.002', 'ether')).to.equal(spent)
+    expect(0).to.equal(minReset)
+    expect(lastReset.gt(0)).to.be.true
+    expect(3).to.equal(nonce)
+  })
 })
 
 async function deployTestToken(minter: SignerWithAddress): Promise<TestToken> {
@@ -188,3 +311,7 @@ async function deployTestToken(minter: SignerWithAddress): Promise<TestToken> {
   )
   return await factory.connect(minter).deploy()
 }
+
+const OneEther = parseUnits('1', 18)
+const AddressZero = '0x'.padEnd(42, '0')
+const Bytes32Zero = '0x'.padEnd(66, '0')
