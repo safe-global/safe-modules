047dfd5b9ff42c30d07b0dbebde5f52a0db07c08
Nicholas Rodrigues Lordello <nick@safe.global>
2023-10-23 13:43:08 +0000

    Skip Coverage for Test Contracts

--- 4337/.solcover.js
+++ 4337/.solcover.js
@@ -1,5 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
+const testDir = path.join(__dirname, 'contracts', 'test');
+const testContracts = fs.readdirSync(testDir)
+    .filter((file) => file.endsWith('.sol'))
+    .map((file) => path.join('test', file));
+
 module.exports = {
-    skipFiles: [],
+    skipFiles: testContracts,
     mocha: {
         grep: '@skip-on-coverage', // Find everything with this tag
         invert: true, // Run the grep's inverse set.
