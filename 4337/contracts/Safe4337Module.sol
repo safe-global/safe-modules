09f38aa6c06e3fbb687e9cd8a0addcc2ab07bd1e
Nicholas Rodrigues Lordello <nick@safe.global>
2023-11-22 10:15:19 +0000

    Revert "Public Operation Data"
    This reverts commit 3e4c4a1d84e8dd31e0cf29ba7ded1f26960e239c.

--- 4337/contracts/Safe4337Module.sol
+++ 4337/contracts/Safe4337Module.sol
@@ -151,7 +151,7 @@ contract Safe4337Module is IAccount, HandlerContext, CompatibilityFallbackHandle
         uint256 maxPriorityFeePerGas,
         uint96 signatureTimestamps,
         address entryPoint
-    ) public view returns (bytes memory) {
+    ) internal view returns (bytes memory) {
         bytes32 safeOperationHash = keccak256(
             abi.encode(
                 SAFE_OP_TYPEHASH,
