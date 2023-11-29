1dd751f70fd363621d9f25d094aa75f7d1a237c3
Shebin John <admin@remedcu.com>
2023-11-29 12:39:58 +0530

    Test ERC721 Token Added

---
+++ 4337/contracts/test/TestERC721Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TestERC721Token is ERC721 {
+    constructor() ERC721("ERC 721 Token", "ERC721") {
+    }
+
+    // @dev This can be called by anyone.
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId, "");
+    }
+}
