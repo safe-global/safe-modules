80d7694b44389db07019bf13fe5aab649fe9933c
Shebin John <admin@remedcu.com>
2023-12-04 14:27:33 +0530

    Alchemy Paymaster Analysis Added

---
+++ paymaster-analysis/alchemy/README.md
@@ -0,0 +1,168 @@
+# Alchemy Paymaster Analysis
+
+## Safe Deployment with Alchemy Paymaster
+
+NPM Command to run:
+```
+npm run alchemy:account        
+
+> @safe-global/aa-analysis@1.0.0 alchemy:account
+> tsx ./alchemy/alchemy-account.ts
+
+Signer Extracted from Private Key.
+
+Init Code Created.
+
+Counterfactual Sender Address Created: 0x66075bF48502407f46cDE90FA9e88b05748A16DA
+Address Link: https://sepolia.etherscan.io/address/0x66075bF48502407f46cDE90FA9e88b05748A16DA
+
+Nonce for the sender received from EntryPoint.
+
+Signed Dummy Data for Paymaster Data Creation from Alchemy.
+
+Received Paymaster Data from Alchemy.
+
+Signed Real Data including Paymaster Data Created by Alchemy.
+
+Safe Account Creation User Operation Successfully Created!
+UserOp Link: https://jiffyscan.xyz/userOpHash/0xd48c4e1fb7f7c8e6b33091ed11e7deb7e1f7bf81ce2840ca9b31f5bef7dda9fb?network=sepolia
+```
+
+Transaction Link: https://sepolia.etherscan.io/tx/https://sepolia.etherscan.io/tx/0x7dfffc6893755ec533cb3488abbc4ed155dccc2d91e22ab86b445ae06ef943aa
+
+Gas Usage: 408370
+
+## Safe Deployment + ERC20 Transaction with Alchemy Paymaster
+
+NPM Command to run:
+```
+npm run alchemy:erc20
+
+> @safe-global/aa-analysis@1.0.0 alchemy:erc20
+> tsx ./alchemy/alchemy-erc20.ts
+
+Signer Extracted from Private Key.
+
+Init Code Created.
+
+Counterfactual Sender Address Created: 0x40726566E5BCb54A99Dc791D075067B5bC188D0D
+Address Link: https://sepolia.etherscan.io/address/0x40726566E5BCb54A99Dc791D075067B5bC188D0D
+
+Safe Wallet ERC20 Balance: 0
+Minting Tokens to Safe Wallet.
+
+Updated Safe Wallet ERC20 Balance: 1
+
+Nonce for the sender received from EntryPoint.
+
+Signed Dummy Data for Paymaster Data Creation from Alchemy.
+
+Received Paymaster Data from Alchemy.
+
+Signed Real Data including Paymaster Data Created by Alchemy.
+
+Safe Account Creation User Operation Successfully Created!
+UserOp Link: https://jiffyscan.xyz/userOpHash/0xa6e7e47f8df520d1a7324fd4fd9e67a521ef6723122a198347fd15dcb0d60dde?network=sepolia
+```
+
+Transaction Link: https://sepolia.etherscan.io/tx/0xd5fa12e541394fe893b068c03d030c4bdcc3ff269de56b20153954246039b8eb
+
+Gas Usage: 419691
+
+## ERC20 Transaction with Alchemy Paymaster
+
+NPM Command to run:
+```
+npm run alchemy:erc20
+
+> @safe-global/aa-analysis@1.0.0 alchemy:erc20
+> tsx ./alchemy/alchemy-erc20.ts
+
+Signer Extracted from Private Key.
+
+Init Code Created.
+
+Counterfactual Sender Address Created: 0xe433Edadd1eD908FE5541366C1F837b9B60162d9
+Address Link: https://sepolia.etherscan.io/address/0xe433Edadd1eD908FE5541366C1F837b9B60162d9
+
+Safe Wallet ERC20 Balance: 1
+
+Nonce for the sender received from EntryPoint.
+
+Signed Dummy Data for Paymaster Data Creation from Alchemy.
+
+Received Paymaster Data from Alchemy.
+
+Signed Real Data including Paymaster Data Created by Alchemy.
+
+Safe Account Creation User Operation Successfully Created!
+UserOp Link: https://jiffyscan.xyz/userOpHash/0xe61b3e204d9dda83e318c17de6dab6eb2fb10bae7786959e93e2c4e1d63df8e8?network=sepolia
+```
+
+Transaction Link: https://sepolia.etherscan.io/tx/0xb2bea2d2ec6b8d9b8cdb62e119a94ff8829b718f6f818e41c2170bba04fb33e2
+
+Gas Usage: 131418
+
+## Safe Deployment + ERC721 Transaction with Alchemy Paymaster
+
+NPM Command to run:
+```
+npm run alchemy:erc721
+
+> @safe-global/aa-analysis@1.0.0 alchemy:erc721
+> tsx ./alchemy/alchemy-erc721.ts
+
+Signer Extracted from Private Key.
+
+Init Code Created.
+
+Counterfactual Sender Address Created: 0x249d03e36b3eDDDB3E862959ef3a1cB28Cf01dD2
+Address Link: https://sepolia.etherscan.io/address/0x249d03e36b3eDDDB3E862959ef3a1cB28Cf01dD2
+
+Nonce for the sender received from EntryPoint.
+
+Signed Dummy Data for Paymaster Data Creation from Alchemy.
+
+Received Paymaster Data from Alchemy.
+
+Signed Real Data including Paymaster Data Created by Alchemy.
+
+Safe Account Creation User Operation Successfully Created!
+UserOp Link: https://jiffyscan.xyz/userOpHash/0x79f121c77c2842d906c9cd193445f73242c08c3a231b49aee654eaecf448689f?network=sepolia
+```
+
+Transaction Link: https://sepolia.etherscan.io/tx/0x24e1f66c2da65d53bef53f70935b270892e451827dc3ed382d4990598aa11eba
+
+Gas Usage: 448953
+
+## ERC721 Transaction with Alchemy Paymaster
+
+NPM Command to run:
+```
+npm run alchemy:erc721
+
+> @safe-global/aa-analysis@1.0.0 alchemy:erc721
+> tsx ./alchemy/alchemy-erc721.ts
+
+Signer Extracted from Private Key.
+
+Init Code Created.
+
+Counterfactual Sender Address Created: 0xe433Edadd1eD908FE5541366C1F837b9B60162d9
+Address Link: https://sepolia.etherscan.io/address/0xe433Edadd1eD908FE5541366C1F837b9B60162d9
+
+Nonce for the sender received from EntryPoint.
+
+Signed Dummy Data for Paymaster Data Creation from Alchemy.
+
+Received Paymaster Data from Alchemy.
+
+Signed Real Data including Paymaster Data Created by Alchemy.
+
+Safe Account Creation User Operation Successfully Created!
+UserOp Link: https://jiffyscan.xyz/userOpHash/0x0f93595c74153ca7d8968ad5206834b53e1a50463835862fe375de472e969420?network=sepolia
+```
+
+Transaction Link: https://sepolia.etherscan.io/tx/0xf002a715320c8ebc181e2debedf86eafd335c5d0163f79d628f994f7023ee8e1
+
+Gas Usage: 194878
---
+++ paymaster-analysis/alchemy/alchemy-erc721.ts
@@ -0,0 +1,256 @@
+import dotenv from "dotenv";
+import { getAccountNonce } from "permissionless";
+import { UserOperation, signUserOperation } from "./utils/userOp";
+import { Address, Hash, createPublicClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { goerli, sepolia } from "viem/chains";
+import { SAFE_ADDRESSES_MAP, encodeCallData, getAccountAddress, getAccountInitCode } from "./utils/safe";
+import { generateMintingCallData } from "../utils/erc721";
+
+dotenv.config()
+const privateKey = process.env.PRIVATE_KEY;
+const ENTRY_POINT_ADDRESS = process.env.ALCHEMY_ENTRYPOINT_ADDRESS;
+const multiSendAddress = process.env.ALCHEMY_MULTISEND_ADDRESS;
+const saltNonce = BigInt(process.env.ALCHEMY_ERC721_PAYMASTER_NONCE);
+const chain = process.env.ALCHEMY_CHAIN;
+const chainID = Number(process.env.ALCHEMY_CHAIN_ID);
+const safeVersion = process.env.SAFE_VERSION;
+const rpcURL = process.env.ALCHEMY_RPC_URL;
+const policyID = process.env.ALCHEMY_GAS_POLICY;
+const apiKey = process.env.ALCHEMY_API_KEY;
+const erc721TokenAddress = process.env.ALCHEMY_ERC721_TOKEN_CONTRACT;
+
+
+if (apiKey === undefined) {
+  throw new Error(
+    "Please replace the `apiKey` env variable with your Alchemy API key"
+  );
+}
+
+if(!privateKey){
+  throw new Error(
+    "Please populate .env file with demo Private Key. Recommended to not use your personal private key."
+  );
+}
+
+const signer = privateKeyToAccount(privateKey as Hash);
+console.log("Signer Extracted from Private Key.");
+
+let publicClient;
+if(chain == "sepolia"){
+  publicClient = createPublicClient({
+    transport: http(rpcURL),
+    chain: sepolia,
+  });
+}
+else if(chain == "goerli"){
+  publicClient = createPublicClient({
+    transport: http(rpcURL),
+    chain: goerli,
+  });
+}
+else {
+  throw new Error(
+    "Current code only support Sepolia and Goerli. Please make required changes if you want to use custom network."
+  )
+}
+
+// The console log in this function could be removed.
+const initCode = await getAccountInitCode({
+  owner: signer.address,
+  addModuleLibAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].ADD_MODULES_LIB_ADDRESS,
+  safe4337ModuleAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].SAFE_4337_MODULE_ADDRESS,
+  safeProxyFactoryAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].SAFE_PROXY_FACTORY_ADDRESS,
+  safeSingletonAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].SAFE_SINGLETON_ADDRESS,
+  saltNonce: saltNonce,
+  multiSendAddress,
+});
+console.log("\nInit Code Created.");
+
+const senderAddress = await getAccountAddress({
+  client: publicClient,
+  owner: signer.address,
+  addModuleLibAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].ADD_MODULES_LIB_ADDRESS,
+  safe4337ModuleAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].SAFE_4337_MODULE_ADDRESS,
+  safeProxyFactoryAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].SAFE_PROXY_FACTORY_ADDRESS,
+  safeSingletonAddress: SAFE_ADDRESSES_MAP[safeVersion][chainID].SAFE_SINGLETON_ADDRESS,
+  saltNonce: saltNonce,
+  multiSendAddress,
+});
+console.log("\nCounterfactual Sender Address Created:", senderAddress);
+if(chain == "sepolia"){
+  console.log("Address Link: https://sepolia.etherscan.io/address/"+senderAddress);
+}
+else if(chain == "goerli"){
+  console.log("Address Link: https://goerli.etherscan.io/address/"+senderAddress);
+}
+else {
+  throw new Error(
+    "Current code only support Sepolia and Goerli. Please make required changes if you want to use custom network."
+  )
+}
+
+const newNonce = await getAccountNonce(publicClient, {
+  entryPoint: ENTRY_POINT_ADDRESS,
+  sender: senderAddress,
+});
+console.log("\nNonce for the sender received from EntryPoint.")
+
+const contractCode = await publicClient.getBytecode({ address: senderAddress });
+
+const sponsoredUserOperation: UserOperation = {
+    sender: senderAddress,
+    nonce: newNonce,
+    initCode: contractCode ? "0x" : initCode,
+    callData: encodeCallData({
+        to: erc721TokenAddress,
+        data: generateMintingCallData(signer.address), // safeMint() function call with corresponding data.
+        value: 0n,
+    }),
+    callGasLimit: 0n, // All Gas Values will be filled by Paymaster Response Data
+    verificationGasLimit: 0n,
+    preVerificationGas: 0n,
+    maxFeePerGas: 0n,
+    maxPriorityFeePerGas: 0n,
+    paymasterAndData: "0x",
+    signature: "0x",
+};
+
+sponsoredUserOperation.signature = await signUserOperation(sponsoredUserOperation, signer);
+console.log("\nSigned Dummy Data for Paymaster Data Creation from Alchemy.")
+
+const gasOptions = {
+  method: 'POST',
+  headers: {accept: 'application/json', 'content-type': 'application/json'},
+  body: JSON.stringify({
+    id: 1,
+    jsonrpc: '2.0',
+    method: 'alchemy_requestGasAndPaymasterAndData',
+    params: [
+      {
+        policyId: policyID,
+        entryPoint: ENTRY_POINT_ADDRESS,
+        dummySignature: sponsoredUserOperation.signature,
+        userOperation: {
+          sender: sponsoredUserOperation.sender,
+          nonce: "0x"+sponsoredUserOperation.nonce.toString(16),
+          initCode: sponsoredUserOperation.initCode,
+          callData: sponsoredUserOperation.callData
+        }
+      }
+    ]
+  })
+};
+
+let responseValues;
+
+if(chain == "sepolia"){
+  await fetch('https://eth-sepolia.g.alchemy.com/v2/'+apiKey, gasOptions)
+  .then(response => response.json())
+  .then(response => responseValues = response)
+  .catch(err => console.error(err));
+}
+else if(chain == "goerli"){
+  await fetch('https://eth-goerli.g.alchemy.com/v2/'+apiKey, gasOptions)
+  .then(response => response.json())
+  .then(response => responseValues = response)
+  .catch(err => console.error(err));
+}
+else {
+  throw new Error(
+    "Current code only support Sepolia and Goerli. Please make required changes if you want to use custom network."
+  )
+}
+console.log("\nReceived Paymaster Data from Alchemy.")
+
+sponsoredUserOperation.preVerificationGas = responseValues.result.preVerificationGas;
+sponsoredUserOperation.preVerificationGas = responseValues.result.preVerificationGas;
+sponsoredUserOperation.callGasLimit = responseValues.result.callGasLimit;
+sponsoredUserOperation.verificationGasLimit = responseValues.result.verificationGasLimit;
+sponsoredUserOperation.paymasterAndData = responseValues.result.paymasterAndData;
+sponsoredUserOperation.maxFeePerGas = responseValues.result.maxFeePerGas;
+sponsoredUserOperation.maxPriorityFeePerGas = responseValues.result.maxPriorityFeePerGas;
+
+sponsoredUserOperation.signature = await signUserOperation(sponsoredUserOperation, signer);
+console.log("\nSigned Real Data including Paymaster Data Created by Alchemy.")
+
+const options = {
+  method: 'POST',
+  headers: {accept: 'application/json', 'content-type': 'application/json'},
+  body: JSON.stringify({
+    id: 1,
+    jsonrpc: '2.0',
+    method: 'eth_sendUserOperation',
+    params: [
+      {
+        sender: sponsoredUserOperation.sender,
+        nonce: "0x"+sponsoredUserOperation.nonce.toString(16),
+        initCode: sponsoredUserOperation.initCode,
+        callData: sponsoredUserOperation.callData,
+        callGasLimit: sponsoredUserOperation.callGasLimit,
+        verificationGasLimit: sponsoredUserOperation.verificationGasLimit,
+        preVerificationGas: sponsoredUserOperation.preVerificationGas,
+        maxFeePerGas: sponsoredUserOperation.maxFeePerGas,
+        maxPriorityFeePerGas: sponsoredUserOperation.maxPriorityFeePerGas,
+        signature: sponsoredUserOperation.signature,
+        paymasterAndData: sponsoredUserOperation.paymasterAndData
+      },
+      ENTRY_POINT_ADDRESS
+    ]
+  })
+};
+
+if(chain == "sepolia"){
+  await fetch('https://eth-sepolia.g.alchemy.com/v2/'+apiKey, options)
+  .then(response => response.json())
+  .then(response => responseValues = response)
+  .catch(err => console.error(err));
+}
+else if(chain == "goerli"){
+  await fetch('https://eth-goerli.g.alchemy.com/v2/'+apiKey, options)
+  .then(response => response.json())
+  .then(response => responseValues = response)
+  .catch(err => console.error(err));
+}
+else {
+  throw new Error(
+    "Current code only support Sepolia and Goerli. Please make required changes if you want to use custom network."
+  )
+}
+
+if(responseValues.result) {
+  console.log("\nSafe Account Creation User Operation Successfully Created!")
+  console.log("UserOp Link: https://jiffyscan.xyz/userOpHash/"+responseValues.result+"?network="+chain+"\n");  
+
+  const hashOptions = {
+    method: 'POST',
+    headers: {accept: 'application/json', 'content-type': 'application/json'},
+    body: JSON.stringify({
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'eth_getUserOperationReceipt',
+      params: [responseValues.result],
+      entryPoint: ENTRY_POINT_ADDRESS,
+    })
+  };
+  let runOnce = true;
+
+  while (responseValues.result == null || runOnce) {
+    await setTimeout(25000);
+    await fetch('https://eth-'+chain+'.g.alchemy.com/v2/'+apiKey, hashOptions)
+      .then(response => response.json())
+      .then(response => responseValues = response)
+      .catch(err => console.error(err));
+    runOnce = false;
+  }
+
+  if(responseValues.result) {
+    console.log("\nTransaction Link: https://"+chain+".etherscan.io/tx/"+responseValues.result.receipt.transactionHash+"\n")
+  }
+  else {
+    console.log("\n"+responseValues.error);
+  }
+}
+else {
+  console.log("\n"+responseValues.error.message);
+}
