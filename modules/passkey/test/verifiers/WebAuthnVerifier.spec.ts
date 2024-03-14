5ddf2d3b510410b363a6a69974d0616101d6591c
Akshay <akshay.ap95@gmail.com>
2024-03-14 08:21:46 +0000

    [#287] Add mock contract as dev dependency, add test for WebAuthnVerifier

--- modules/passkey/test/verifiers/WebAuthnVerifier.spec.ts
+++ modules/passkey/test/verifiers/WebAuthnVerifier.spec.ts
@@ -1,7 +1,8 @@
 import { expect } from 'chai'
 import { deployments, ethers } from 'hardhat'
+import hre from 'hardhat'
 
-describe.only('WebAuthnVerifier', () => {
+describe('WebAuthnVerifier', () => {
   const setupTests = deployments.createFixture(async ({ deployments }) => {
     const { WebAuthnVerifier } = await deployments.fixture()
     const webAuthnVerifier = await ethers.getContractAt('WebAuthnVerifier', WebAuthnVerifier.address)
@@ -74,5 +75,32 @@ describe.only('WebAuthnVerifier', () => {
         0n,
       ),
     ).to.be.false
+  })
+
+  it('Should return true on successful signature checks', async () => {
+    const mockP256Verifier = await (await hre.ethers.getContractFactory('MockContract')).deploy()
+    await mockP256Verifier.givenAnyReturnBool(true)
+    const webAuthnVerifier = await (await hre.ethers.getContractFactory('WebAuthnVerifier')).deploy(mockP256Verifier.target)
+
+    const authenticatorData = ethers.randomBytes(100)
+    authenticatorData[32] = 0x01
+
+    const challenge = ethers.randomBytes(32)
+    const clientDataFields = ethers.randomBytes(100)
+
+    expect(await webAuthnVerifier.verifyWebAuthnSignature(authenticatorData, '0x01', challenge, clientDataFields, [0n, 0n], 0n, 0n)).to.be
+      .true
+
+    expect(
+      await webAuthnVerifier.verifyWebAuthnSignatureAllowMalleability(
+        authenticatorData,
+        '0x01',
+        challenge,
+        clientDataFields,
+        [0n, 0n],
+        0n,
+        0n,
+      ),
+    ).to.be.true
   })
 })
