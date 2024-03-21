a275baffb530c2615859bf19fe642a81e413a363
Mikhail Mikheev <16622558+mmv08@users.noreply.github.com>
2024-03-21 15:19:01 +0100

    Fix the assertion about the S Value in the p256 signer class

--- modules/passkey/test/verifiers/P256VerifierLib.spec.ts
+++ modules/passkey/test/verifiers/P256VerifierLib.spec.ts
@@ -20,7 +20,7 @@ describe('P256VerifierLib', function () {
   it('Should return true on valid signature', async function () {
     const { verifier, verifierLib, account } = await setupTests()
 
-    const message = ethers.id('hello world')
+    const message = ethers.id('hello passkeys')
     const { r, s } = account.sign(message)
     const { x, y } = account.publicKey
 
@@ -36,7 +36,7 @@ describe('P256VerifierLib', function () {
   it('Should check for signature signature malleability', async function () {
     const { verifier, verifierLib, account } = await setupTests()
 
-    const message = ethers.id('hello world')
+    const message = ethers.id('hello passkeys')
     const { r, highS } = account.sign(message)
     const { x, y } = account.publicKey
 
@@ -47,7 +47,7 @@ describe('P256VerifierLib', function () {
   it('Should return false for misbehaving verifiers', async function () {
     const { verifierLib, account } = await setupTests()
 
-    const message = ethers.id('hello world')
+    const message = ethers.id('hello passkeys')
     const { r, s } = account.sign(message)
     const { x, y } = account.publicKey
 
