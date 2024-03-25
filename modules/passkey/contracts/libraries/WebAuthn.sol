43882d025411b8a1021ca20e0c7e13d8e066d788
Mikhail Mikheev <16622558+mmv08@users.noreply.github.com>
2024-03-25 11:58:08 +0100

    Remove vendored library

--- modules/passkey/contracts/libraries/WebAuthn.sol
+++ modules/passkey/contracts/libraries/WebAuthn.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import {Base64Url} from "../vendor/FCL/utils/Base64Url.sol";
 import {IP256Verifier, P256} from "./P256.sol";
 
 /**
