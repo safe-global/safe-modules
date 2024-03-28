2093cd0ef983b45eb2877a62f2e304b86e950a7b
Akshay <akshay.ap95@gmail.com>
2024-03-28 16:32:56 +0100

    Deploy Safe in setup

--- modules/passkey/test/userStories/executeUserOpFromPasskeySigner.ts
+++ modules/passkey/test/userStories/executeUserOpFromPasskeySigner.ts
@@ -5,6 +5,7 @@ import { WebAuthnCredentials, decodePublicKey, encodeWebAuthnSignature } from '.
 import { Safe4337Module } from '@safe-global/safe-4337/typechain-types/contracts/Safe4337Module'
 import { buildSafeUserOpTransaction, buildPackedUserOperationFromSafeUserOperation } from '@safe-global/safe-4337/src/utils/userOp'
 import { buildSignatureBytes } from '@safe-global/safe-4337/src/utils/execution'
+import { Log } from 'hardhat-deploy/types'
 
 /**
  * User story: Execute userOp from Passkey signer
@@ -13,10 +14,8 @@ import { buildSignatureBytes } from '@safe-global/safe-4337/src/utils/execution'
  *
  * The flow can be summarized as follows:
  * Step 1: Setup the contracts.
- * Step 2: Create a userOp, sign it with Passkey signer.
- * Step 3: Execute the userOp that deploys a safe with passkey signer as owner.
- * Step 4: Create a userOp and sign it using passkey credential.
- * Step 5: Execute the userOp.
+ * Step 2: Create a userOp and sign it using passkey credential.
+ * Step 3: Execute the userOp.
  */
 describe('Execute userOp from Passkey signer [@User story]', () => {
   const setupTests = deployments.createFixture(async ({ deployments }) => {
@@ -56,8 +55,31 @@ describe('Execute userOp from Passkey signer [@User story]', () => {
 
     // Deploy a signer contract
     const publicKey = decodePublicKey(credential.response)
-    await (await signerFactory.createSigner(publicKey.x, publicKey.y, await verifier.getAddress())).wait()
+    await signerFactory.createSigner(publicKey.x, publicKey.y, await verifier.getAddress())
     const signer = await signerFactory.getSigner(publicKey.x, publicKey.y, await verifier.getAddress())
+
+    // Deploy a Safe with passkey signer as owner
+
+    // The initializer data to enable the Safe4337Module as a module on a Safe
+    const initializer = safeModuleSetup.interface.encodeFunctionData('enableModules', [[module.target]])
+
+    // Create setup data to deploy a Safe with EOA and passkey signer as owners, threshold 1, Safe4337Module as module and fallback handler
+    const setupData = singleton.interface.encodeFunctionData('setup', [
+      [signer],
+      1n,
+      safeModuleSetup.target,
+      initializer,
+      module.target,
+      ethers.ZeroAddress,
+      0,
+      ethers.ZeroAddress,
+    ])
+
+    // Deploy a Safe with EOA and passkey signer as owners
+    const safeSalt = Date.now()
+    const txReceipt = await (await proxyFactory.createProxyWithNonce(singleton, setupData, safeSalt)).wait()
+    // Get Safe address from the event logs
+    const safeAddress = txReceipt.logs.filter((log: Log) => log.address === proxyFactory.target)[0].args[0]
 
     return {
       user,
@@ -72,107 +94,21 @@ describe('Execute userOp from Passkey signer [@User story]', () => {
       SafeL2,
       signer,
       credential,
+      safeAddress,
     }
   })
 
   it('should execute a userOp with WebAuthn signer as owner', async () => {
     // Step 1: Setup the contracts
-    const { user, proxyFactory, safeModuleSetup, module, entryPoint, singleton, navigator, SafeL2, signer, credential } = await setupTests()
-
-    // Step 2: Create a userOp, sign it with Passkey signer.
-    const safeSalt = Date.now()
-    const initializer = safeModuleSetup.interface.encodeFunctionData('enableModules', [[module.target]])
-
-    const setupData = singleton.interface.encodeFunctionData('setup', [
-      [signer],
-      1n,
-      await safeModuleSetup.getAddress(),
-      initializer,
-      await module.getAddress(),
-      ethers.ZeroAddress,
-      0,
-      ethers.ZeroAddress,
-    ])
-
-    // Predict the Safe address to construct the userOp
-    const safe = await proxyFactory.createProxyWithNonce.staticCall(singleton, setupData, safeSalt)
-
-    const deployData = proxyFactory.interface.encodeFunctionData('createProxyWithNonce', [singleton.target, setupData, safeSalt])
-
-    // Create safeOp
-    const safeOp = buildSafeUserOpTransaction(
-      safe,
-      user.address,
-      ethers.parseEther('0.0'),
-      '0x',
-      await entryPoint.getNonce(safe, 0),
-      await entryPoint.getAddress(),
-      false,
-      true,
-      {
-        initCode: ethers.solidityPacked(['address', 'bytes'], [proxyFactory.target, deployData]),
-        verificationGasLimit: 700000,
-      },
-    )
-
-    const packedUserOp = buildPackedUserOperationFromSafeUserOperation({
-      safeOp,
-      signature: '0x',
-    })
-
-    const opHash = await module.getOperationHash(packedUserOp)
-
-    const assertion = navigator.credentials.get({
-      publicKey: {
-        challenge: ethers.getBytes(opHash),
-        rpId: 'safe.global',
-        allowCredentials: [{ type: 'public-key', id: new Uint8Array(credential.rawId) }],
-        userVerification: 'required',
-      },
-    })
-
-    // Build contract signature that the Safe will forward to the signer contract
-    const signature = buildSignatureBytes([
-      {
-        signer: signer as string,
-        data: encodeWebAuthnSignature(assertion.response),
-        dynamic: true,
-      },
-    ])
-
-    // Send 1 ETH to the Safe
-    await user.sendTransaction({ to: safe, value: ethers.parseEther('1') }).then((tx) => tx.wait())
-    // Check if Safe is not already created
-    expect(await ethers.provider.getCode(safe)).to.equal('0x')
-
-    // Step 3: Execute the userOp that deploys a safe with passkey signer as owner.
-    await (
-      await entryPoint.handleOps(
-        [
-          {
-            ...packedUserOp,
-            signature: ethers.solidityPacked(['uint48', 'uint48', 'bytes'], [safeOp.validAfter, safeOp.validUntil, signature]),
-          },
-        ],
-        user.address,
-      )
-    ).wait()
-
-    // Check if Safe is created and uses the expected Singleton
-    const [implementation] = ethers.AbiCoder.defaultAbiCoder().decode(['address'], await ethers.provider.getStorage(safe, 0))
-    expect(implementation).to.equal(singleton.target)
-
-    // Check the owners of the created Safe
-    const safeInstance = await ethers.getContractAt(SafeL2.abi, safe)
-    expect(await safeInstance.getOwners()).to.deep.equal([signer])
+    const { user, module, entryPoint, navigator, signer, credential, safeAddress } = await setupTests()
 
     // Step 4: Create a userOp and sign it using passkey credential.
     const safeOp2 = buildSafeUserOpTransaction(
-      safe,
+      safeAddress,
       ethers.ZeroAddress,
       ethers.parseEther('0.2'),
       '0x',
-      await entryPoint.getNonce(safe, 0),
+      await entryPoint.getNonce(safeAddress, 0),
       await entryPoint.getAddress(),
       false,
       true,
@@ -206,6 +142,10 @@ describe('Execute userOp from Passkey signer [@User story]', () => {
     const balanceBefore = await ethers.provider.getBalance(ethers.ZeroAddress)
 
     // Step 5: Execute the userOp.
+
+    // Send 1 ETH to the Safe
+    await user.sendTransaction({ to: safeAddress, value: ethers.parseEther('1') }).then((tx) => tx.wait())
+
     await (
       await entryPoint.handleOps(
         [
@@ -220,5 +160,6 @@ describe('Execute userOp from Passkey signer [@User story]', () => {
 
     // Check if the address(0) the 0.2 ETH
     expect(await ethers.provider.getBalance(ethers.ZeroAddress)).to.be.equal(balanceBefore + ethers.parseEther('0.2'))
+    expect(await ethers.provider.getBalance(safeAddress)).to.be.lessThanOrEqual(ethers.parseEther('1') - ethers.parseEther('0.2'))
   })
 })
