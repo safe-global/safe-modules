eaedf0a47d746713f60b0997c4ce61a34ea1d498
Nicholas Rodrigues Lordello <nick@safe.global>
2024-04-04 12:25:05 +0200

    Base-64 Encode Challenge Directly Into JSON String
    This PR encodes the 32 byte challenge directly into the client data JSON
    that needs to be computed for the signing message. This allows the code
    to avoid copy loops when computing the client data JSON that would be
    required pre-`mcopy` opcode if the challenge were encoded to a separate
    base-64 string.
    
    This change has some nice small improvements to the overall cost of the
    verifying signatures, and more substantial improvements to the
    deployment cost of the signer.

--- modules/passkey/contracts/test/TestWebAuthnLib.sol
+++ modules/passkey/contracts/test/TestWebAuthnLib.sol
@@ -5,11 +5,18 @@ import {WebAuthn} from "../libraries/WebAuthn.sol";
 import {IP256Verifier} from "../libraries/P256.sol";
 
 contract TestWebAuthnLib {
+    function encodeClientDataJson(
+        bytes32 challenge,
+        string calldata clientDataFields
+    ) external pure returns (string memory clientDataJson) {
+        clientDataJson = WebAuthn.encodeClientDataJson(challenge, clientDataFields);
+    }
+
     function signingMessage(
         bytes32 challenge,
         bytes calldata authenticatorData,
         string calldata clientDataFields
-    ) public pure returns (bytes32 message) {
+    ) external pure returns (bytes32 message) {
         message = WebAuthn.signingMessage(challenge, authenticatorData, clientDataFields);
     }
 
@@ -20,7 +27,7 @@ contract TestWebAuthnLib {
         uint256 x,
         uint256 y,
         IP256Verifier verifier
-    ) public view returns (bool success) {
+    ) external view returns (bool success) {
         success = WebAuthn.verifySignature(challenge, signature, authenticatorFlags, x, y, verifier);
     }
 
@@ -31,7 +38,7 @@ contract TestWebAuthnLib {
         uint256 x,
         uint256 y,
         IP256Verifier verifier
-    ) public view returns (bool success) {
+    ) external view returns (bool success) {
         success = WebAuthn.verifySignature(challenge, signature, authenticatorFlags, x, y, verifier);
     }
 }
