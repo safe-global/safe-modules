27cf804374be8adaf6c1eae08f224c1c6923b282
Akshay <akshay.ap95@gmail.com>
2024-04-12 13:58:08 +0200

    Fix imports in user story

--- modules/passkey/test/userstories/ExecuteWithPaymaster.spec.ts
+++ modules/passkey/test/userstories/ExecuteWithPaymaster.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import { deployments, ethers } from 'hardhat'
-import { WebAuthnCredentials, decodePublicKey, encodeWebAuthnSignature } from '../utils/webauthn'
+import { decodePublicKey, encodeWebAuthnSignature } from '../../src/utils/webauthn'
+import { WebAuthnCredentials } from '../../test/utils/webauthnShim'
 import { buildSafeUserOpTransaction, buildPackedUserOperationFromSafeUserOperation } from '@safe-global/safe-4337/dist/src/utils/userOp'
 import { buildSignatureBytes } from '@safe-global/safe-4337/dist/src/utils/execution'
 
