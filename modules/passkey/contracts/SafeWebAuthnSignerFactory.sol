9427756b8a2d93980d032852d888ab08ad2a4956
Akshay <akshay.ap95@gmail.com>
2024-04-22 15:02:57 +0200

    [#312] Fix variable name

--- modules/passkey/contracts/SafeWebAuthnSignerFactory.sol
+++ modules/passkey/contracts/SafeWebAuthnSignerFactory.sol
@@ -68,7 +68,7 @@ contract SafeWebAuthnSignerFactory is ISafeSignerFactory {
             let dataSize := mload(data)
             let dataLocation := add(data, 0x20)
 
-            let success := staticcall(gas(), _singleton, dataLocation, dataSize, 0, 0)
+            let success := staticcall(gas(), singleton, dataLocation, dataSize, 0, 0)
             if eq(success, 1) {
                 returndatacopy(0, 0, returndatasize())
                 return(0, returndatasize())
