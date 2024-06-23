99c9c6c32288b6aedbe97695bd038e2363516ccd
yoavelmalem <yoav.el@certora.com>
2024-06-23 11:39:42 +0300

    Added getConfiguration Rule

--- certora/harnesses/Utilities.sol
+++ certora/harnesses/Utilities.sol
@@ -1,3 +1,10 @@
+import {P256} from "../../modules/passkey/contracts/libraries/WebAuthn.sol";
+
+
+interface IConfigHolder {
+    function getConfiguration() external pure returns (uint256 x, uint256 y, P256.Verifiers verifiers);
+}
+
 contract Utilities {
     function havocAll() external {
         (bool success, ) = address(0xdeadbeef).call(abi.encodeWithSelector(0x12345678));
@@ -6,5 +13,9 @@ contract Utilities {
 
     function justRevert() external {
         revert();
+    }
+
+    function getConfiguration(address proxy) external view returns (uint256 x, uint256 y, P256.Verifiers verifiers) {
+        return IConfigHolder(proxy).getConfiguration();
     }
 }
