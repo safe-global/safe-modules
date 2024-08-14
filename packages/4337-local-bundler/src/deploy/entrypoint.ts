5ae188e04b6d69e6500f73cc1fc628f1ac14f0d5
Nicholas Rodrigues Lordello <nick@safe.global>
2024-08-14 12:04:20 +0200

    Use Arachnid CREATE2 Deployer For Some Contracts
    Fixes #481
    
    Some contracts included in our deployment script are expected to use the
    Arachnid CREATE deployment proxy instead of the Safe singleton factory.
    We change our deployment scripts to manually use the Arachnid
    CREATE2 deployer in those specific cases, so the contracts end up at the
    expected addresses.
    
    Affected contracts:
    - ERC-4337 entry point
    - Daimo P-256 verifier

--- packages/4337-local-bundler/src/deploy/entrypoint.ts
+++ packages/4337-local-bundler/src/deploy/entrypoint.ts
@@ -1,7 +1,9 @@
 import EntryPoint from '@account-abstraction/contracts/artifacts/EntryPoint.json'
-import { DeployFunction } from 'hardhat-deploy/types'
+import type { HardhatRuntimeEnvironment } from 'hardhat/types'
+import type { DeployFunction } from 'hardhat-deploy/types'
 
-const deploy: DeployFunction = async ({ deployments, getNamedAccounts, network }) => {
+const deploy: DeployFunction = async (hre) => {
+  const { deployments, getNamedAccounts, network } = hre
   if (!network.tags.entrypoint) {
     return
   }
@@ -9,12 +11,28 @@ const deploy: DeployFunction = async ({ deployments, getNamedAccounts, network }
   const { deployer } = await getNamedAccounts()
   const { deploy } = deployments
 
-  await deploy('EntryPoint', {
-    from: deployer,
-    contract: EntryPoint,
-    args: [],
-    log: true,
-    deterministicDeployment: '0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3',
+  // The official ERC-4337 entry point contract is deployed using the Arachnid CREATE2 deployer.
+  await withArachnidDeterministicDeploymentProxy(hre, () =>
+    deploy('EntryPoint', {
+      from: deployer,
+      contract: EntryPoint,
+      args: [],
+      log: true,
+      deterministicDeployment: '0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3',
+    }),
+  )
+}
+
+function withArachnidDeterministicDeploymentProxy<T>({ config }: HardhatRuntimeEnvironment, f: () => Promise<T>): Promise<T> {
+  // This is a bit hacky - but the `hardhat-deploy` package reads that deterministic deployment
+  // configuration before deploying each contract. This means that we can temporarily override the
+  // the configuration to `undefined` so that it uses the (default) Arachnid deterministic
+  // deployment proxy for a specific deployment, and then restore the configuration afterwards so
+  // that remaining contracts use the deterministic deployment factory from `hardhat.config.ts`.
+  const existingConfig = config.deterministicDeployment
+  config.deterministicDeployment = undefined
+  return f().finally(() => {
+    config.deterministicDeployment = existingConfig
   })
 }
 
